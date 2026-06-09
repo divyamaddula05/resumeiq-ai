23b7405a5b931e392014b4199d0198246b2c56f9
divyamaddula05 <divyamaddula9876@gmail.com>
2026-06-09 23:37:16 +0530

    Fixed os.getlogin deployment issue

--- App/App.py
+++ App/App.py
@@ -351,7 +351,11 @@ def run():
         sec_token = secrets.token_urlsafe(12)
         host_name = socket.gethostname()
         ip_add = socket.gethostbyname(host_name)
-        dev_user = os.getlogin()
+        try:
+            dev_user = os.getlogin()
+
+        except:
+            dev_user = "cloud_user"
         os_name_ver = platform.system() + " " + platform.release()
         g = geocoder.ip('me')
         latlong = g.latlng
